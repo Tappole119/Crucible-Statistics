3d27a3aa777327a61406555a9674a929acac0e1f
Tappole119 <mruben@uccs.edu>
2017-06-14 17:55:31 -0600

    db seeds for 3 test players

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Player.destroy_all
+
+Player.create!([{
+    DisplayName: "RecordedLunick",
+    MemberId: "4611686018429800327",
+    Membertype: "1",
+    CharacterId: "2305843009215100127",
+    Playertype: "Response"
+    },
+
+	{
+    DisplayName: "DrBaker117",
+    MemberId: "4611686018429817613",
+    Membertype: "1",
+    CharacterId: "2305843009216799794",
+    Playertype: "Response"
+    },
+
+	{
+    DisplayName: "WhiteThunder",
+    MemberId: "4611686018429800327",
+    Membertype: "1",
+    CharacterId: "2305843009215101435",
+    Playertype: "Response"
+    }
+    ])
+
+p "Created #{Player.count} players"
