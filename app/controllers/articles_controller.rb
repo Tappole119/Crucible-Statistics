01d8d57f27714c7a5163cb6c9c35af8f2b29b689
Tappole119 <mruben@uccs.edu>
2017-06-14 17:02:40 -0600

    Article changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,6 @@ class ArticlesController < ApplicationController
   
   private
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:gamertag, :psnid)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+	attr_accessor :gamertag,:psnid
+
+	validates :gamertag, presence: true,length: { minimum: 5 }
 end
