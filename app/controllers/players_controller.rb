b5b33502a8ea76eb2d22a0210635d2ee667f84ff
Tappole119 <mruben@uccs.edu>
2017-06-14 18:50:54 -0600

    Search functionality

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,6 +5,7 @@ class PlayersController < ApplicationController
   # GET /players.json
   def index
     @players = Player.all
+    @players = Player.search(params[:search])
   end
 
   # GET /players/1
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,9 @@
 class Player < ApplicationRecord
+	def self.search(search)
+		if search
+			self.where("displayname like ?", "%#{search}%")
+		else
+			self.all
+		end
+	end
 end
