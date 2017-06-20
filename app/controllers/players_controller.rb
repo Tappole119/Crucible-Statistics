4e7be64698ed04a425d81f0194e7b689b6619103
Tappole119 <mruben@uccs.edu>
2017-06-19 18:40:21 -0600

    mrge res

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,7 +5,7 @@ class PlayersController < ApplicationController
   # GET /players.json
   def index
     @players = Player.all
-    @players = Player.search(params[:search])
+    @players = Player.search(params[:search], params[:name])
   end
 
   # GET /players/1
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ApplicationRecord
-	def self.search(search)
+	def self.search(search, name)
 		if search
-			self.where("displayname like ?", "%#{search}%")
+      		self.where("displayname like ? and membertype like ?", "%#{search}%", "%#{name}%")
 		else
 			self.all
 		end
